413b424b9d52dfb4a171ff4d1e3bde4f133c813e
root <root@DESKTOP-ICHREQA.localdomain>
2021-11-04 11:52:24 +0100

    Improved speed / lower memory usage of orbit calculations

--- config.h
+++ config.h
@@ -2,6 +2,6 @@
 
 #include <cstdint>
 
-inline constexpr int64_t k = 4;
+inline constexpr int64_t k = 5;
 inline constexpr int64_t dimension = 1 << k;
 inline constexpr int64_t n_nodes = 1LL << dimension;
--- orbit.h
+++ orbit.h
@@ -67,8 +67,8 @@ struct OrbitInfo {
 
     // Compute how many neighbours of the checked type that nodes of different
     // types have
-    std::vector<std::set<int>> seenNeighbourCounts(numNodeTypes);
-    std::map<uint32_t, int> seenRepresentativeCounts;
+    std::vector<std::set<uint8_t>> seenNeighbourCounts(numNodeTypes);
+    std::vector<uint8_t> seenRepresentativeCounts((*nodeRepresentatives.rbegin()) + 1);
     for (auto i : nodeRepresentatives) {
       auto seen = numberOfNeighboursOfType((uint32_t)i);
       seenNeighbourCounts[nodeType[i]].insert(seen);
@@ -80,10 +80,11 @@ struct OrbitInfo {
     std::vector<std::map<int, uint8_t>> countToNewNodeType(numNodeTypes);
     for (size_t type = 0; type < countToNewNodeType.size(); type++) {
       auto it = seenNeighbourCounts[type].begin();
-      countToNewNodeType[type][*it] = (int8_t)type;
+      countToNewNodeType[type][*it] = (uint8_t)type;
       for (it++; it != seenNeighbourCounts[type].end(); it++) {
         countToNewNodeType[type][*it] = numNodeTypes;
         numNodeTypes++;
+        assert(numNodeTypes != 0);
       }
     }
 
@@ -113,35 +114,44 @@ struct OrbitInfo {
       linearBasis.add((-chi(S)).getIndex());
     }
 
-    for (uint32_t node = 0; node < n_nodes; ++node)
+    for (long long node = 0; node < n_nodes; ++node) {
       nodeRepresentatives.insert(linearBasis.reduce(node));
+    }
+
+    std::cout << "Max size of node representative " << *std::max_element(nodeRepresentatives.begin(), nodeRepresentatives.end()) << std::endl;
 
     std::cout << "Number of node representatives " << nodeRepresentatives.size() << std::endl;
 
     // Start by splitting the set of nodes based on whether they belong to Z
     numNodeTypes = 2;
-    nodeType = std::vector<uint8_t>(n_nodes, 1);
+    nodeType = std::vector<uint8_t>((*nodeRepresentatives.rbegin()) + 1, 1);
     nodeType[0] = 0;
 
     for (int checkType = 0; checkType < numNodeTypes; ++checkType) {
       // Split sets of nodes based on how many neighbours they have of type
       // checkType
-      splitBasedOnType(checkType);  
+      std::cout << "Checking type " << checkType << " out of " << (long long)numNodeTypes << std::endl;
+      splitBasedOnType(checkType); 
     }
+    
+    std::cout << "Orbits found: " << (long long)numNodeTypes << std::endl;
+
+    std::vector<uint8_t>fullNodeType(n_nodes);
 
     for (uint32_t S = 0; S < dimension; ++S) {
       uint32_t base = chi(S).getIndex();
       uint32_t base2 = (-chi(S)).getIndex();
       for (auto node : nodeRepresentatives) {
-        nodeType[node ^ base] = nodeType[node];
-        nodeType[node ^ base2] = nodeType[node];
+        fullNodeType[node ^ base] = nodeType[node];
+        fullNodeType[node ^ base2] = nodeType[node];
       }
     }
+    nodeType = move(fullNodeType);
 
     calcAllNodes();
     calcNodeOrbits();
 
-    std::cout << "Orbits found " << nodeOrbits.size() << std::endl;
+    //std::cout << "Orbits found " << nodeOrbits.size() << std::endl;
     //for (auto orbit : nodeOrbits)
     //std::cout << "Orbit size " << orbit.size() << std::endl;
   }
